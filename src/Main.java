264d807b7f28333b439431ba0eedf10d79ae5d2d
vixitru <128ahanrahan@frhsd.com>
2026-09-14 12:23:59 -0400

    Reply 3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, I am MF");
         System.out.println("Hi MF, I am EY");
+        System.out.println("Hi MF and EY, I am A.H.");
     }
 }
